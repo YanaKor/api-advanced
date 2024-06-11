a24f52d97ca01702ef9095da340ba567e4f00fb6
Yana Kormshchikova <veryutina.yana@yandex.ru>
2024-06-12 00:15:06 +0300

    фикс данных для теста

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -11,7 +11,7 @@ def test_post_v1_account():
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'ya_kor_test3'
+    login = 'ya_kor_test4'
     email = f'{login}@mail.ru'
     password = '32545gtth'
 
