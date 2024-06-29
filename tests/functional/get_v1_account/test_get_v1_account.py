e0cc9fa78484c2b5f4db7a04d9237855dcf35429
Yana Kormshchikova <veryutina.yana@yandex.ru>
2024-06-29 19:58:49 +0300

    добавлены функции чекеры

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,33 +1,15 @@
 from datetime import datetime
 from assertpy import assert_that, soft_assertions
-# from hamcrest import assert_that, has_property, starts_with, all_of, instance_of, has_properties, equal_to
 
 from checkers.http_checkers import check_status_code_http
 from dm_api_account.models.user_details_envelope import UserRole
+from checkers.get_v1_account import GetV1Account
 
 
-#
-# def test_get_v1_account(auth_account_api):
-#     response = auth_account_api.dm_account_api.account_api.get_v1_account()
-#     assert_that(
-#         response, all_of(
-#             has_property('resource', has_property('login', starts_with('ya_kor'))),
-#             has_property('resource', has_property('online', instance_of(datetime))),
-#             has_property(
-#                 'resource', has_properties(
-#                     {
-#                         'rating': has_properties(
-#                             {
-#                                 "enabled": equal_to(True),
-#                                 "quality": equal_to(0),
-#                                 "quantity": equal_to(0)
-#                             }
-#                         )
-#                     }
-#                 )
-#             )
-#         )
-#     )
+def test_get_v1_account(auth_account_api):
+    response = auth_account_api.dm_account_api.account_api.get_v1_account()
+    GetV1Account.check_response_values(response)
+
 
 def test_get_v1_account_auth(auth_account_api):
     response = auth_account_api.dm_account_api.account_api.get_v1_account()
